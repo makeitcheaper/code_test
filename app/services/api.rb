8c6339705df79a9ac0e7a6db90d37afcf53b306a
Andy White <andy@wireworldmedia.co.uk>
2018-09-16 09:29:37 +0100

    Api service handles success and error conditions

--- app/services/api.rb
+++ app/services/api.rb
@@ -2,13 +2,17 @@ require 'httparty'
 
 class Api
   def self.enqueue(token:, user_id:, acc_name:, partner_name:, name:, business_name:, tel_number:, email:)
-    path = '/api/v1/create'
-    url = ENV['LEAD_API_URI'] + path
-    resp = HTTParty.post(
-      url,
-      body: body(token, user_id, acc_name, partner_name, name, business_name, tel_number, email),
-      headers: { 'Content-Type' => 'application/x-www-form-urlencoded' }
-    ).tap {|r| puts ">>>>> response: #{r.parsed_response}"}
+    Response.new(
+      HTTParty.post(
+        url,
+        body: body(token, user_id, acc_name, partner_name, name, business_name, tel_number, email),
+        headers: { 'Content-Type' => 'application/x-www-form-urlencoded' }
+      )
+    )
+  end
+
+  def self.url
+    ENV['LEAD_API_URI'] + '/api/v1/create'
   end
 
   private
@@ -29,5 +33,16 @@ class Api
         reference: ''
       }
     )
+  end
+
+  class Response
+    attr_reader :status, :message, :errors
+
+    def initialize(httparty_response)
+      @status = httparty_response.code
+      parsed_response = httparty_response.parsed_response
+      @message = parsed_response['message']
+      @errors = parsed_response['errors']
+    end
   end
 end
--- spec/services/api_spec.rb
+++ spec/services/api_spec.rb
@@ -1,24 +1,48 @@
 require_relative '../../app/services/api'
 
 describe Api do
-  before { WebMock.allow_net_connect! }
+  before do
+    stub_request(:post, Api.url)
+      .to_return(status: status, body: body,
+                 headers: {'Content-Type'=>'application/json'})
+  end
 
-  context 'should works' do
-    let(:result) do
-      Api.enqueue(
-        token: ENV['LEAD_API_ACCESS_TOKEN'],
-        user_id: ENV['LEAD_API_PGUID'],
-        acc_name: ENV['LEAD_API_PACCNAME'],
-        partner_name: ENV['LEAD_API_PPARTNER'],
-        name: 'Peter Pan',
-        business_name: 'Widgets Co',
-        tel_number: '01273677688',
-        email: 'anyone@example.com'
-      )
-    end
+  let(:body) do
+    { message: message, errors: errors }.to_json
+  end
 
-    it 'expect it to work' do
-      expect(result).to eq 22
-    end
+  let(:result) do
+    Api.enqueue(
+      token: ENV['LEAD_API_ACCESS_TOKEN'],
+      user_id: ENV['LEAD_API_PGUID'],
+      acc_name: ENV['LEAD_API_PACCNAME'],
+      partner_name: ENV['LEAD_API_PPARTNER'],
+      name: 'Peter Pan',
+      business_name: 'Widgets Co',
+      tel_number: '01273677688',
+      email: 'anyone@example.com'
+    )
+  end
+
+  shared_examples 'expected response' do
+    it { expect(result.status).to eq status }
+    it { expect(result.message).to eq message }
+    it { expect(result.errors).to eq errors }
+  end
+
+  context 'success response' do
+    let(:status) { 201 }
+    let(:message) { 'Enqueue success' }
+    let(:errors) { [] }
+
+    it_behaves_like 'expected response'
+  end
+
+  context 'error response' do
+    let(:status) { 400 }
+    let(:message) { 'There was an error' }
+    let(:errors) { ['an error occurred'] }
+
+    it_behaves_like 'expected response'
   end
 end
