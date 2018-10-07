ff41ac1c2ff5ad8c8f9f80988435520f84922edb
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2018-10-08 00:09:27 +0100

    Finish leads submission spec

--- spec/features/leads_submission_spec.rb
+++ spec/features/leads_submission_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'Leads submission', type: :feature do
@@ -15,8 +17,9 @@ RSpec.describe 'Leads submission', type: :feature do
 
     context 'when MIC API is available' do
       before do
-        # expect call to API and return success
-        click_button 'Submit'
+        VCR.use_cassette('mic-api-201') do
+          click_button 'Submit'
+        end
       end
 
       it 'displays a thank you message' do
@@ -24,14 +27,28 @@ RSpec.describe 'Leads submission', type: :feature do
       end
     end
 
+    context 'when MIC API validation fails' do
+      before do
+        fill_in 'Customer full name', with: 'Bob'
+        VCR.use_cassette('mic-api-400') do
+          click_button 'Submit'
+        end
+      end
+
+      it 'displays errors returned from MIC API' do
+        expect(page).to have_content("'name' must be composed with 2 separated words (space between)")
+      end
+    end
+
     context 'when MIC API is not available' do
       before do
-        # expect call to API and return error
-        click_button 'Submit'
+        VCR.use_cassette('mic-api-401') do
+          click_button 'Submit'
+        end
       end
 
       it 'presents the user with a nice message' do
-        expect(page).to have_content('The service is not available at the moment. Please try again later.')
+        expect(page).to have_content('Internal app error, try again later.')
       end
     end
   end
@@ -42,7 +59,7 @@ RSpec.describe 'Leads submission', type: :feature do
     end
 
     it 'validates form fields' do
-      expect(page).to have_content('There was an error submitting the form.')
+      expect(page).to have_content('Please review the problems below:')
     end
   end
 end
