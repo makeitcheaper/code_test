86dad418c9c2843dda729d2fdeb834e3db5e9602
Herj Pulahi <herjit@ryaltoapp.com>
2018-10-08 13:48:48 +0100

    Update tests for Lead attributes

--- spec/models/lead_spec.rb
+++ spec/models/lead_spec.rb
@@ -1,7 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe Lead, type: :model do
-  before { @lead = FactoryBot.build(:lead) }
+describe Lead do
+
+  before do
+    @lead = Lead.new(name: "Example User", email: "user@example.com",
+                     business_name: "foobar", telephone_number: "01344123456",
+                     contact_time: DateTime.now.to_formatted_s(:db))
+  end
 
   subject { @lead }
 
@@ -13,14 +18,66 @@ RSpec.describe Lead, type: :model do
   it { should respond_to(:notes) }
   it { should respond_to(:reference) }
 
-  it { is_expected.to validate_presence_of(:name).with_message('Name is required') }
-  it { is_expected.to validate_presence_of(:email).with_message('Email invalid') }
-  it { is_expected.to validate_presence_of(:business_name).with_message('Business name is required') }
-  it { is_expected.to validate_presence_of(:telephone_number).with_message('Telephone number is required') }
-
-  it { is_expected.to validate_length_of(:business_name).is_at_most(100).with_message("Business name can contain max 100 chars") }
-  it { is_expected.to validate_length_of(:reference).is_at_most(50).with_message("Reference can contain 50 chars") }
-  it { is_expected.to validate_length_of(:notes).is_at_most(255).with_message("Notes can contain max 255 chars") }
-
   it { should be_valid }
+
+  describe "when name is not present" do
+    before { @lead.name = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when email is not present" do
+    before { @lead.email = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when name is too long" do
+    before { @lead.name = "a" * 101 }
+    it { should_not be_valid }
+  end
+
+  describe "when email format is invalid" do
+    it "should be invalid" do
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+                     foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+      addresses.each do |invalid_address|
+        @lead.email = invalid_address
+        expect(@lead).not_to be_valid
+      end
+    end
+  end
+
+  describe "when email format is valid" do
+    it "should be valid" do
+      addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+      addresses.each do |valid_address|
+        @lead.email = valid_address
+        expect(@lead).to be_valid
+      end
+    end
+  end
+
+  describe "when business name is not present" do
+    before { @lead.business_name = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when business name is too long" do
+    before { @lead.business_name = "a" * 101 }
+    it { should_not be_valid }
+  end
+
+  describe "when telephone number is not present" do
+    before { @lead.telephone_number = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when notes is too long" do
+    before { @lead.telephone_number = "a" * 51}
+    it { should_not be_valid }
+  end
+
+  describe "when reference is too long" do
+    before { @lead.telephone_number = "a" * 256}
+    it { should_not be_valid }
+  end
 end
