d150b5333434a996c203bfdbc1b37d935130b237
Herj Pulahi <herjit@ryaltoapp.com>
2018-10-09 16:09:47 +0100

    Rescue from exceptions

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,4 +1,8 @@
 class LeadsController < ApplicationController
+  rescue_from LeadApiService::InternalServerError, with: :internal_server_error
+  rescue_from LeadApiService::UnauthorizedAccessToken, with: :unauthorized_access_token
+  rescue_from LeadApiService::BadRequest, with: :bad_request
+
   def new
     @lead = Lead.new
   end
@@ -20,5 +24,20 @@ class LeadsController < ApplicationController
     params.require(:lead).permit(:name, :business_name,
                                  :email, :telephone_number, :contact_time, :reference,
                                  :notes, :access_token, :pGUID, :pAccName, :pPartner)
+  end
+
+  def internal_server_error
+    flash.now[:danger] = 'There was an internal server error'
+    render 'new'
+  end
+
+  def unauthorized_access_token
+    flash.now[:danger] = 'There was an internal problem. we will look into right away '
+    render 'new'
+  end
+
+  def bad_request
+    flash.now[:danger] = 'There was an internal problem. A bad request occured'
+    render 'new'
   end
 end
--- app/services/lead_api_service.rb
+++ app/services/lead_api_service.rb
@@ -2,13 +2,29 @@ class LeadApiService
   include HTTParty
   base_uri ENV['LEAD_API_URI']
 
-  attr_reader :lead
+  class InternalServerError < StandardError; end
+  class BadRequest < StandardError; end
+  class UnauthorizedAccessToken < StandardError; end
+
+  attr_reader :lead, :response
 
   def initialize(lead)
     @lead = lead
   end
 
   def call
+    @response = self.class.post('/api/v1/create', body: lead.as_json)
+    raise service_error unless response.success?
+  end
+
+  private
+
+
+  def service_error
+    "LeadApiService::#{response.response.message.parameterize(separator: '_').classify}".constantize
+  end
+
+  def send_external_request
     self.class.post('/api/v1/create', body: lead.as_json)
   end
 end
