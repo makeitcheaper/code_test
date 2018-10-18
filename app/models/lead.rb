5035557ee8243d5dc3828544a1ff790df2a43862
thomasnal <thomas.nalevajko@gmail.com>
2018-10-18 11:19:46 +0100

    Add to_h and submit

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -12,4 +12,17 @@ class Lead
   validates_presence_of :full_name, :business_name, :email, :phone_number
   validates :email, email: true
   validates :phone_number, phone: true
+
+
+  def to_h
+    self.class.attributes.inject({}) do |hash, key|
+      hash.merge({ key => self.send(key) })
+    end
+  end
+
+
+  def submit
+    api = LeadApi.new
+    api.post_lead to_h
+  end
 end
