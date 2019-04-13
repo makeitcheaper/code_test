354552bbff5b8aa7f04fce7a9bc73fc613f57c50
Barbara Slawinska <slawinskabarbara@gmail.com>
2019-04-13 23:31:57 +0100

    validation tests

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,4 +1,12 @@
 class Lead
   include ActiveModel::Model
   attr_accessor :name, :business_name, :telephone_number, :email
+  validates :name, presence: true
+  validates :name, length: { maximum: 100 }
+  validates :business_name, presence: true
+  validates :business_name, length: { maximum: 100 }
+  validates :telephone_number, presence: true
+  validates :telephone_number, length: { maximum: 13 }
+  validates :email, presence: true
+  validates :email, length: { maximum: 80 }
 end
--- spec/models/lead_spec.rb
+++ spec/models/lead_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Lead do
 
   let!(:lead) do
    Lead.new(name: 'John Smith', business_name: 'Bank', 
-   telephone_number: '+44 2222 333 444', email: 'smith@bank.co.uk')
+   telephone_number: '+442222333444', email: 'smith@bank.co.uk')
   end
 
   it 'is valid' do 
@@ -14,7 +14,48 @@ RSpec.describe Lead do
   it 'has right attributes' do
     expect(lead.name).to eq 'John Smith'
     expect(lead.business_name).to eq 'Bank'
-    expect(lead.telephone_number).to eq '+44 2222 333 444'
+    expect(lead.telephone_number).to eq '+442222333444'
     expect(lead.email).to eq 'smith@bank.co.uk'
   end
+
+  it 'requires name' do
+    lead.name = nil
+    expect(lead).to_not be_valid
+  end
+
+  it 'requires name to have max 100 chars' do
+    lead.name = 'J' * 101 
+    expect(lead).to_not be_valid
+  end
+
+  it 'requires business name' do
+    lead.business_name = nil
+    expect(lead).to_not be_valid
+  end
+
+  it 'requires business name to have max 100 chars' do
+    lead.business_name = 'B' * 101 
+    expect(lead).to_not be_valid
+  end
+
+  it 'requires telephone number' do
+    lead.telephone_number = nil
+    expect(lead).to_not be_valid
+  end
+
+  it 'requires telephone number to have max 13 chars' do
+    lead.telephone_number = '1' * 14 
+    expect(lead).to_not be_valid
+  end
+
+  it 'requires email' do
+    lead.email = nil
+    expect(lead).to_not be_valid
+  end
+
+    it 'requires email to have max 80 chars' do
+    lead.email = 's' * 81 
+    expect(lead).to_not be_valid
+  end
+
 end
