998241c399d2200d86d59dc9fa2117e496dd7805
Yuri Tomanek <38002+yuritomanek@users.noreply.github.com>
2019-05-06 15:16:47 +0100

    Added notes and reference

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,12 +5,14 @@ require 'securerandom'
 class Customer
   include ActiveModel::Model
 
-  attr_accessor :uid, :name, :business_name, :telephone_number, :email
+  attr_accessor :uid, :name, :business_name, :telephone_number, :email, :notes, :reference
 
   validates :uid, :name, :business_name, :telephone_number, :email, presence: true
   validates :name, :business_name, length: { maximum: 100 }
   validates :telephone_number, length: { maximum: 13 }, format: { with: VALID_PHONE_NUMBERS, multiline: true }
   validates :email, length: { maximum: 80 }, format: { with: VALID_EMAILS }
+  validates :notes, length: { maximum: 255 }
+  validates :reference, length: { maximum: 50 }
 
   def initialize(attributes = {})
     super
