23b0c6a01b5750c7ed3e67e53a9d6cb16df79700
Mateusz Luterek <mateusz.luterek@lunarlogic.io>
2019-06-02 23:00:05 +0200

    Update Rspec config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,7 +9,7 @@ require 'rspec/rails'
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
-ActiveRecord::Migration.maintain_test_schema!
+# ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
